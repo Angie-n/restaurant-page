0b1c0f264ddcb2a9a794c0deb7beb0b408fc6f5b
Angie Nguyen <angie.ta.nguyen@gmail.com>
2022-03-19 16:59:18 -0700

    Added rules for loading images and fonts

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,16 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
